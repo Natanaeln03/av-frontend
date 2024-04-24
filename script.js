f1aa45317a693e2f65a4f7ffd637e749f42278da
Natanaeln03 <natancnascimento88@gmail.com>
2024-04-24 10:50:14 -0300

    modificado

---
+++ script.js
@@ -0,0 +1,26 @@
+//Seleciona todos os elementos co a class product-box
+const boxes = document.querySelectorAll(".product-box")
+
+//literação sobre cada elemento box
+boxes.forEach(box => {
+   //Enconter o elemento img dentro do elemento box atual
+   const img = box.querySelector(".product-image")
+
+   //Adicionar os ouvintes de eventos ao elemento box atual
+   box.addEventListener("mousemove", (e) => {
+    const x = e.clientX = box.getBoundingClientRect().left
+    const y = e.clientY = box.getBoundingClientRect().top
+
+    console.log(x, y)
+
+    img.style.transformorigin = `${x}px ${y}px`
+    img.style.transform = "scale(3)"
+   })
+
+   box.addEventListener("mauseleave", (e) => {
+
+    img.style.transformorigin = "center"
+    img.style.transform = "scale(1)"
+   })
+})
+
